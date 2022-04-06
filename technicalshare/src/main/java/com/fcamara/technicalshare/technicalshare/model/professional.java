7196351c54c941a91ea0bb25b152da7f68618ef3
Beatriz Paixão <biaperon2001@gmail.com>
2022-04-06 10:36:07 -0300

    Iniciando model professional

---
+++ technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/model/professional.java
@@ -0,0 +1,11 @@
+package com.fcamara.technicalshare.technicalshare.model;
+
+public class professional {
+	private Integer id;
+	private String occupation;
+	private Integer duration;
+	private String description;
+	private String experience_level;
+	
+
+}
