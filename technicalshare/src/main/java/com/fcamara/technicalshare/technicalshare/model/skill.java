4ce0e4c99448341d72849c76171ba9aaf60e819a
brunosas88 <bruno.sas@live.com>
2022-04-06 10:25:56 -0300

    criando classe skill.java para representar as habilidades do usuario cadastrado no sistema

---
+++ technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/model/skill.java
@@ -0,0 +1,8 @@
+package com.fcamara.technicalshare.technicalshare.model;
+
+public class skill {
+    private Integer id;
+    private String skill;
+    private String subcategory;
+    private String category;
+}
