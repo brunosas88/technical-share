80d9fd57280ed85b9e03acecb6b77c6679492aa9
Beatriz Paixão <biaperon2001@gmail.com>
2022-04-06 15:28:12 -0300

    Criando Model AcademicEducation

---
+++ technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/AcademicEducation/model/AcademicEducation.java
@@ -0,0 +1,40 @@
+package com.fcamara.technicalshare.technicalshare.AcademicEducation.model;
+
+import java.time.LocalDate;
+
+import com.fcamara.technicalshare.technicalshare.Profile.model.Profile;
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="academic_education")
+public class AcademicEducation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="course")
+    private String course;
+
+    @Column(name="start_date")
+    private LocalDate startDate;
+
+    @Column(name="final_date")
+    private LocalDate finalDate;
+
+    @Column(name = "institution")
+    private String institution;
+
+    @ManyToOne
+	@JoinColumn(name = "id_user")
+	private Profile profile;
+    
+}
--- technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/academic_education/model/academic_education.java
+++
@@ -1,5 +0,0 @@
-package com.fcamara.technicalshare.technicalshare.academic_education.model;
-
-public class academic_education {
-
-}
