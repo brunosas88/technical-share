c9c14ad275361578855f48512387f4a0dbc5da0a
Beatriz Paixão <biaperon2001@gmail.com>
2022-04-06 14:28:38 -0300

    Organização Repositório

--- technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/academic_education/academic_education.java
+++
@@ -1,5 +0,0 @@
-package com.fcamara.technicalshare.technicalshare.academic_education;
-
-public class academic_education {
-
-}
---
+++ technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/profile/profile.java
@@ -0,0 +1,5 @@
+package com.fcamara.technicalshare.technicalshare.profile;
+
+public class profile {
+    private String name;
+}
