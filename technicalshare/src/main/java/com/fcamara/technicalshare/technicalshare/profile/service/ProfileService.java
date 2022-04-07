56e914ab80637d9c29601c15ac581256bf888741
brunosas88 <bruno.sas@live.com>
2022-04-07 09:28:08 -0300

    Estrutura do ProfileController.java e ProfileService.java criado

--- technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/profile/service/ProfileService.java
+++ technicalshare/src/main/java/com/fcamara/technicalshare/technicalshare/profile/service/ProfileService.java
@@ -1,9 +1,7 @@
 package com.fcamara.technicalshare.technicalshare.profile.service;
 
-import com.fcamara.technicalshare.technicalshare.profile.model.Profile;
 import com.fcamara.technicalshare.technicalshare.profile.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +9,7 @@ import org.springframework.stereotype.Service;
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+
 
 
 
